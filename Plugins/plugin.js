ca509411b43fdf882983047abe25c84280edd523
ABOODI-249 <sting7429@gmail.com>
2026-03-05 07:45:23 +0200

    Update plugin.js

--- Plugins/plugin.js
+++ Plugins/plugin.js
@@ -16,13 +16,13 @@ module.exports = {
   alias: [...mergedCommands],
   uniquecommands: ["install", "uninstall", "plugins", "pluginlist"],
   description: "Install, Uninstall, List plugins",
-  start: async (Atlas, m, { text, args, pushName, prefix, inputCMD, isCreator, isintegrated, doReact }) => {
+  start: async (DarkLegend, m, { text, args, pushName, prefix, inputCMD, isCreator, isintegrated, doReact, botImage1 }) => {
     switch (inputCMD) {
       case "install":
         chechSenderModStatus = await checkMod(m.sender);
         if (!chechSenderModStatus && !isCreator && !isintegrated) {
           await doReact("❌");
-          return Atlas.sendMessage(m.from, {
+          return DarkLegend.sendMessage(m.from, {
             text: `Sorry, only *Owners* and *Mods* can use this command !`,
             quoted: m,
           });
@@ -31,7 +31,7 @@ module.exports = {
           var url = new URL(text);
         } catch (e) {
           console.log(e);
-          return await client.sendMessage(
+          return await DarkLegend.sendMessage(
             m.from,
             { text: `Invalid URL !` },
             { quoted: m }
@@ -50,17 +50,13 @@ module.exports = {
             var folderName = "Plugins";
             fileName = path.basename(url);
 
-            // check if plugin is already installed and present in that Database array
             plugin = await isPluginPresent(fileName);
             if (plugin) {
               return m.reply(`*${fileName}* plugin is already Installed !`);
             }
 
-            // Check if that file is present in same directory
             if (fs.existsSync(`./Plugins/${fileName}`)) {
-              return m.reply(
-                `*${fileName}* plugin is already Present Locally !`
-              );
+              return m.reply(`*${fileName}* plugin is already Present Locally !`);
             }
 
             var filePath = path.join(folderName, fileName);
@@ -80,7 +76,7 @@ module.exports = {
         await doReact("🧩");
         const plugins = await getAllPlugins();
         if (!plugins.length) {
-          await Atlas.sendMessage(
+          await DarkLegend.sendMessage(
             m.from,
             { text: `No additional plugins installed !` },
             { quoted: m }
@@ -91,24 +87,21 @@ module.exports = {
             txt += `🔖 *Plugin ${i+1}*\n*🎀 Name:* ${plugins[i].plugin}\n*🧩 Url:* ${plugins[i].url}\n\n`;
           }
           txt += `⚜️ To uninstall a plugin type *uninstall* plugin-name !\n\nExample: *${prefix}uninstall* audioEdit.js`;
-          await Atlas.sendMessage(m.from, { text: txt }, { quoted: m });
+          await DarkLegend.sendMessage(m.from, { text: txt }, { quoted: m });
         }
-
         break;
 
       case "uninstall":
         chechSenderModStatus = await checkMod(m.sender);
         if (!chechSenderModStatus && !isCreator && !isintegrated) {
           await doReact("❌");
-          return Atlas.sendMessage(m.from, {
+          return DarkLegend.sendMessage(m.from, {
             text: `Sorry, only *Owners* and *Mods* can use this command !`,
             quoted: m,
           });
         }
         if (!text) {
-          return await m.reply(
-            `Please provide a plugin name !\n\nExample: *${prefix}uninstall* audioEdit.js`
-          );
+          return await m.reply(`Please provide a plugin name !\n\nExample: *${prefix}uninstall* audioEdit.js`);
         }
         await doReact("🧩");
         fileName = text;
@@ -123,31 +116,29 @@ module.exports = {
           fs.unlinkSync(`./Plugins/${fileName}`);
           await delPlugin(fileName);
           await readcommands();
-          await m.reply(
-            `*${fileName}* plugin uninstalled successfully !\n\nPlease restart the bot to clear cache !`
-          );
+          await m.reply(`*${fileName}* plugin uninstalled successfully !\n\nPlease restart the bot to clear cache !`);
         } else {
           await doReact("❌");
           return m.reply(`*${fileName}* plugin is not installed !`);
         }
-
         break;
 
-        case "pluginlist":
-          await doReact("🧩");
-          textssf = `*『    Installable Plugins List    』*\n\n
-*🎀 Name:* audioEdit.js\n🔖 *Number of commads:* 8\n*🧩 Url:* https://gist.githubusercontent.com/FantoX001/b818960e024c541e155f948db34a2da2/raw/f6771fbd4c615a64eafb92d53e7627276f20167a/audio-edit.js\n\n
-*🎀 Name:* text-to-speech.js\n🔖 *Number of commads:* 7\n*🧩 Url:* https://gist.githubusercontent.com/FantoX001/109e3f04e70ca2edeb8d47072bbd0499/raw/84de4d44994fcb8b9f315a2be41eac062378df01/text-to-speech.js\n\n
-*🎀 Name:* image-edit.js\n🔖 *Number of commads:* 4\n*🧩 Url:* https://gist.githubusercontent.com/FantoX001/b48fd5040b2cd83e5e331c0d2c974871/raw/909c5a6a32cfcb2dbb965f1ee2a5e3025802de5b/image-edit.js\n\n     
-*🎀 Name:* logo-maker.js\n🔖 *Number of commads:* 40\n*🧩 Url:* https://gist.githubusercontent.com/FantoX001/b8e4a9782623c6197c10f68aa798a548/raw/7466871764434cf4c2ee30b15aac871e5db48a74/logo-maker.js\n\n 
-*🎀 Name:* fun.js\n🔖 *Number of commads:* 17\n*🧩 Url:* https://gist.githubusercontent.com/FantoX001/e4df3eb3cc06baaccce3130a29262b30/raw/4c9b280fe527891f4d935b36b1c06e7f2fda9f6f/fun.js\n\n
+      case "pluginlist":
+        await doReact("🧩");
+        textssf = `*『    Installable Plugins List    』*\n\n
+*🎀 Name:* audioEdit.js\n🔖 *Number of commands:* 8\n*🧩 Url:* https://gist.githubusercontent.com/FantoX001/b818960e024c541e155f948db34a2da2/raw/f6771fbd4c615a64eafb92d53e7627276f20167a/audio-edit.js\n\n
+*🎀 Name:* text-to-speech.js\n🔖 *Number of commands:* 7\n*🧩 Url:* https://gist.githubusercontent.com/FantoX001/109e3f04e70ca2edeb8d47072bbd0499/raw/84de4d44994fcb8b9f315a2be41eac062378df01/text-to-speech.js\n\n
+*🎀 Name:* image-edit.js\n🔖 *Number of commands:* 4\n*🧩 Url:* https://gist.githubusercontent.com/FantoX001/b48fd5040b2cd83e5e331c0d2c974871/raw/909c5a6a32cfcb2dbb965f1ee2a5e3025802de5b/image-edit.js\n\n
+*🎀 Name:* logo-maker.js\n🔖 *Number of commands:* 40\n*🧩 Url:* https://gist.githubusercontent.com/FantoX001/b8e4a9782623c6197c10f68aa798a548/raw/7466871764434cf4c2ee30b15aac871e5db48a74/logo-maker.js\n\n
+*🎀 Name:* fun.js\n🔖 *Number of commands:* 17\n*🧩 Url:* https://gist.githubusercontent.com/FantoX001/e4df3eb3cc06baaccce3130a29262b30/raw/4c9b280fe527891f4d935b36b1c06e7f2fda9f6f/fun.js\n\n
 *🎀 Name:* chat-GPT.js\n🔖 *Number of commands:* 2\n*🧩 Url:* https://gist.githubusercontent.com/FantoX001/ec3e327c9711b1d3059cc26b8b7945be/raw/9396030969cbf0f24ad1c318a9035540ce4577b2/chat-GPT.js\n\n
 *🎀 Name:* tiktokdl.js\n🔖 *Number of commands:* 4\n*🧩 Url:* https://gist.githubusercontent.com/FantoX001/481b039ef502a56339374b29b7491695/raw/854ed660349cc3fd45de89ce137721c674a03ec3/tiktokdl.js\n\n
 *🎀 Name:* nsfw-image.js\n🔖 *Number of commands:* 1\n*🧩 Url:* https://gist.githubusercontent.com/FantoX001/804c106f1f2fb1ae46e9bd63f854069d/raw/a93191b83c0cca44abb7e0e26b55caf2892f0bb4/nsfw-image.js\n\n
 
 ⚜️ To install a plugin type *install* _plugin-url_ !\n\nExample: *${prefix}install* https://gist.githubusercontent.com/FantoX001/xyz...\n\n⚜️ To uninstall a plugin type *uninstall* _plugin-name_ !\n\nExample: *${prefix}uninstall* audioEdit.js\n`;
-          await Atlas.sendMessage(m.from, { image: {url: botImage1},caption: textssf }, { quoted: m });
-          break;
+        await DarkLegend.sendMessage(m.from, { image: { url: botImage1 }, caption: textssf }, { quoted: m });
+        break;
+
       default:
         break;
     }
