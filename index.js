24548259f01ca64b7e2d74314ee61fcff3cecf0e
ABOODI-249 <sting7429@gmail.com>
2026-03-05 07:41:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("./Configurations");
 const {
-  default: atlasConnect,
+  default: darklegendConnect,
   DisconnectReason,
   fetchLatestBaileysVersion,
   downloadContentFromMessage,
@@ -27,7 +27,7 @@ const mongoose = require("mongoose");
 const Auth = require("./System/MongoAuth/MongoAuth");
 const qrcode = require("qrcode");
 const {
-  getPluginURLs, // -------------------- GET ALL PLUGIN DATA FROM DATABASE
+  getPluginURLs,
 } = require("./System/MongoDB/MongoDb_Core.js");
 
 const chalk = require("chalk");
@@ -38,10 +38,11 @@ const store = makeInMemoryStore({
   }),
 });
 
-// Atlas Server configuration
+// DarkLegend Server configuration
 let QR_GENERATE = "invalid";
 let status;
-const startAtlas = async () => {
+
+const startDarkLegend = async () => {
   try {
     await mongoose.connect(mongodb).then(() => {
       console.log(
@@ -56,11 +57,12 @@ const startAtlas = async () => {
     );
     console.log(err);
   }
-  const { getAuthFromDatabase } = new Auth(sessionId);
 
+  const { getAuthFromDatabase } = new Auth(sessionId);
   const { saveState, state, clearState } = await getAuthFromDatabase();
+
   console.log(
-    figlet.textSync("ATLAS", {
+    figlet.textSync("DARKLEGEND", {
       font: "Standard",
       horizontalLayout: "default",
       vertivalLayout: "default",
@@ -68,23 +70,24 @@ const startAtlas = async () => {
       whitespaceBreak: true,
     })
   );
+
   console.log(`\n`);
 
   await installPlugin();
 
-  const { version, isLatest } = await fetchLatestBaileysVersion();
+  const { version } = await fetchLatestBaileysVersion();
 
-  const Atlas = atlasConnect({
+  const DarkLegend = darklegendConnect({
     logger: pino({ level: "silent" }),
     printQRInTerminal: true,
-    browser: ["Atlas", "Safari", "1.0.0"],
+    browser: ["DarkLegend", "Safari", "1.0.0"],
     auth: state,
     version,
   });
 
-  store.bind(Atlas.ev);
+  store.bind(DarkLegend.ev);
 
-  Atlas.public = true;
+  DarkLegend.public = true;
 
   async function installPlugin() {
     console.log(chalk.yellow("Checking for Plugins...\n"));
@@ -102,7 +105,7 @@ const startAtlas = async () => {
 
     if (!plugins.length || plugins.length == 0) {
       console.log(
-        chalk.redBright("No Extra Plugins Installed ! Starting Atlas...\n")
+        chalk.redBright("No Extra Plugins Installed ! Starting DarkLegend...\n")
       );
     } else {
       console.log(
@@ -125,7 +128,7 @@ const startAtlas = async () => {
       }
       console.log(
         chalk.greenBright(
-          "All Plugins Installed Successfully ! Starting Atlas...\n"
+          "All Plugins Installed Successfully ! Starting DarkLegend...\n"
         )
       );
     }
@@ -133,300 +136,77 @@ const startAtlas = async () => {
 
   await readcommands();
 
-  Atlas.ev.on("creds.update", saveState);
-  Atlas.serializeM = (m) => smsg(Atlas, m, store);
-  Atlas.ev.on("connection.update", async (update) => {
+  DarkLegend.ev.on("creds.update", saveState);
+  DarkLegend.serializeM = (m) => smsg(DarkLegend, m, store);
+
+  DarkLegend.ev.on("connection.update", async (update) => {
     const { lastDisconnect, connection, qr } = update;
+
     if (connection) {
-      console.info(`[ ATLAS ] Server Status => ${connection}`);
+      console.info(`[ DARKLEGEND ] Server Status => ${connection}`);
     }
 
     if (connection === "close") {
       let reason = new Boom(lastDisconnect?.error)?.output.statusCode;
+
       if (reason === DisconnectReason.badSession) {
-        console.log(
-          `[ ATLAS ] Bad Session File, Please Delete Session and Scan Again.\n`
-        );
+        console.log(`[ DARKLEGEND ] Bad Session File, Please Delete Session and Scan Again.\n`);
         process.exit();
       } else if (reason === DisconnectReason.connectionClosed) {
-        console.log("[ ATLAS ] Connection closed, reconnecting....\n");
-        startAtlas();
+        console.log("[ DARKLEGEND ] Connection closed, reconnecting....\n");
+        startDarkLegend();
       } else if (reason === DisconnectReason.connectionLost) {
-        console.log("[ ATLAS ] Connection Lost from Server, reconnecting...\n");
-        startAtlas();
+        console.log("[ DARKLEGEND ] Connection Lost from Server, reconnecting...\n");
+        startDarkLegend();
       } else if (reason === DisconnectReason.connectionReplaced) {
-        console.log(
-          "[ ATLAS ] Connection Replaced, Another New Session Opened, Please Close Current Session First!\n"
-        );
+        console.log("[ DARKLEGEND ] Connection Replaced, Another New Session Opened!\n");
         process.exit();
       } else if (reason === DisconnectReason.loggedOut) {
         clearState();
-        console.log(
-          `[ ATLAS ] Device Logged Out, Please Delete Session and Scan Again.\n`
-        );
+        console.log(`[ DARKLEGEND ] Device Logged Out, Please Delete Session and Scan Again.\n`);
         process.exit();
       } else if (reason === DisconnectReason.restartRequired) {
-        console.log("[ ATLAS ] Server Restarting...\n");
-        startAtlas();
+        console.log("[ DARKLEGEND ] Server Restarting...\n");
+        startDarkLegend();
       } else if (reason === DisconnectReason.timedOut) {
-        console.log("[ ATLAS ] Connection Timed Out, Trying to Reconnect...\n");
-        startAtlas();
-      } else {
-        console.log(
-          `[ ATLAS ] Server Disconnected: "It's either safe disconnect or WhatsApp Account got banned !\n"`
-        );
+        console.log("[ DARKLEGEND ] Connection Timed Out, Trying to Reconnect...\n");
+        startDarkLegend();
       }
     }
+
     if (qr) {
       QR_GENERATE = qr;
     }
   });
 
-  Atlas.ev.on("group-participants.update", async (m) => {
-    welcomeLeft(Atlas, m);
+  DarkLegend.ev.on("group-participants.update", async (m) => {
+    welcomeLeft(DarkLegend, m);
   });
 
-  Atlas.ev.on("messages.upsert", async (chatUpdate) => {
-    m = serialize(Atlas, chatUpdate.messages[0]);
+  DarkLegend.ev.on("messages.upsert", async (chatUpdate) => {
+    m = serialize(DarkLegend, chatUpdate.messages[0]);
 
     if (!m.message) return;
     if (m.key && m.key.remoteJid == "status@broadcast") return;
     if (m.key.id.startsWith("BAE5") && m.key.id.length == 16) return;
 
-    require("./Core.js")(Atlas, m, commands, chatUpdate);
+    require("./Core.js")(DarkLegend, m, commands, chatUpdate);
   });
-
-  Atlas.getName = (jid, withoutContact = false) => {
-    id = Atlas.decodeJid(jid);
-    withoutContact = Atlas.withoutContact || withoutContact;
-    let v;
-    if (id.endsWith("@g.us"))
-      return new Promise(async (resolve) => {
-        v = store.contacts[id] || {};
-        if (!(v.name || v.subject)) v = Atlas.groupMetadata(id) || {};
-        resolve(
-          v.name ||
-            v.subject ||
-            PhoneNumber("+" + id.replace("@s.whatsapp.net", "")).getNumber(
-              "international"
-            )
-        );
-      });
-    else
-      v =
-        id === "0@s.whatsapp.net"
-          ? {
-              id,
-              name: "WhatsApp",
-            }
-          : id === Atlas.decodeJid(Atlas.user.id)
-          ? Atlas.user
-          : store.contacts[id] || {};
-    return (
-      (withoutContact ? "" : v.name) ||
-      v.subject ||
-      v.verifiedName ||
-      PhoneNumber("+" + jid.replace("@s.whatsapp.net", "")).getNumber(
-        "international"
-      )
-    );
-  };
-
-  Atlas.decodeJid = (jid) => {
-    if (!jid) return jid;
-    if (/:\d+@/gi.test(jid)) {
-      let decode = jidDecode(jid) || {};
-      return (
-        (decode.user && decode.server && decode.user + "@" + decode.server) ||
-        jid
-      );
-    } else return jid;
-  };
-
-  Atlas.ev.on("contacts.update", (update) => {
-    for (let contact of update) {
-      let id = Atlas.decodeJid(contact.id);
-      if (store && store.contacts)
-        store.contacts[id] = {
-          id,
-          name: contact.notify,
-        };
-    }
-  });
-
-  Atlas.downloadAndSaveMediaMessage = async (
-    message,
-    filename,
-    attachExtension = true
-  ) => {
-    let quoted = message.msg ? message.msg : message;
-    let mime = (message.msg || message).mimetype || "";
-    let messageType = message.mtype
-      ? message.mtype.replace(/Message/gi, "")
-      : mime.split("/")[0];
-    const stream = await downloadContentFromMessage(quoted, messageType);
-    let buffer = Buffer.from([]);
-    for await (const chunk of stream) {
-      buffer = Buffer.concat([buffer, chunk]);
-    }
-    let type = await FileType.fromBuffer(buffer);
-    trueFileName = attachExtension ? filename + "." + type.ext : filename;
-    // save to file
-    await fs.writeFileSync(trueFileName, buffer);
-    return trueFileName;
-  };
-
-  Atlas.downloadMediaMessage = async (message) => {
-    let mime = (message.msg || message).mimetype || "";
-    let messageType = message.mtype
-      ? message.mtype.replace(/Message/gi, "")
-      : mime.split("/")[0];
-    const stream = await downloadContentFromMessage(message, messageType);
-    let buffer = Buffer.from([]);
-    for await (const chunk of stream) {
-      buffer = Buffer.concat([buffer, chunk]);
-    }
-
-    return buffer;
-  };
-
-  Atlas.parseMention = async (text) => {
-    return [...text.matchAll(/@([0-9]{5,16}|0)/g)].map(
-      (v) => v[1] + "@s.whatsapp.net"
-    );
-  };
-
-  Atlas.sendText = (jid, text, quoted = "", options) =>
-    Atlas.sendMessage(
-      jid,
-      {
-        text: text,
-        ...options,
-      },
-      {
-        quoted,
-      }
-    );
-
-  Atlas.getFile = async (PATH, save) => {
-    let res;
-    let data = Buffer.isBuffer(PATH)
-      ? PATH
-      : /^data:.*?\/.*?;base64,/i.test(PATH)
-      ? Buffer.from(PATH.split`,`[1], "base64")
-      : /^https?:\/\//.test(PATH)
-      ? await (res = await getBuffer(PATH))
-      : fs.existsSync(PATH)
-      ? ((filename = PATH), fs.readFileSync(PATH))
-      : typeof PATH === "string"
-      ? PATH
-      : Buffer.alloc(0);
-
-    let type = (await FileType.fromBuffer(data)) || {
-      mime: "application/octet-stream",
-      ext: ".bin",
-    };
-    filename = path.join(
-      __filename,
-      "../src/" + new Date() * 1 + "." + type.ext
-    );
-    if (data && save) fs.promises.writeFile(filename, data);
-    return {
-      res,
-      filename,
-      size: await getSizeMedia(data),
-      ...type,
-      data,
-    };
-  };
-
-  Atlas.setStatus = (status) => {
-    Atlas.query({
-      tag: "iq",
-      attrs: {
-        to: "@s.whatsapp.net",
-        type: "set",
-        xmlns: "status",
-      },
-      content: [
-        {
-          tag: "status",
-          attrs: {},
-          content: Buffer.from(status, "utf-8"),
-        },
-      ],
-    });
-    return status;
-  };
-
-  Atlas.sendFile = async (jid, PATH, fileName, quoted = {}, options = {}) => {
-    let types = await Atlas.getFile(PATH, true);
-    let { filename, size, ext, mime, data } = types;
-    let type = "",
-      mimetype = mime,
-      pathFile = filename;
-    if (options.asDocument) type = "document";
-    if (options.asSticker || /webp/.test(mime)) {
-      let { writeExif } = require("./lib/sticker.js");
-      let media = {
-        mimetype: mime,
-        data,
-      };
-      pathFile = await writeExif(media, {
-        packname: global.packname,
-        author: global.packname,
-        categories: options.categories ? options.categories : [],
-      });
-      await fs.promises.unlink(filename);
-      type = "sticker";
-      mimetype = "image/webp";
-    } else if (/image/.test(mime)) type = "image";
-    else if (/video/.test(mime)) type = "video";
-    else if (/audio/.test(mime)) type = "audio";
-    else type = "document";
-    await Atlas.sendMessage(
-      jid,
-      {
-        [type]: {
-          url: pathFile,
-        },
-        mimetype,
-        fileName,
-        ...options,
-      },
-      {
-        quoted,
-        ...options,
-      }
-    );
-    return fs.promises.unlink(pathFile);
-  };
 };
 
-startAtlas();
+startDarkLegend();
 
 app.use("/", express.static(join(__dirname, "Frontend")));
 
 app.get("/qr", async (req, res) => {
   const { session } = req.query;
+
   if (!session)
-    return void res
-      .status(404)
-      .setHeader("Content-Type", "text/plain")
-      .send("Please Provide the session ID that you set for authentication !")
-      .end();
+    return res.send("Please Provide the session ID !");
+
   if (sessionId !== session)
-    return void res
-      .status(404)
-      .setHeader("Content-Type", "text/plain")
-      .send("Invalid session ID ! Please check your session ID !")
-      .end();
-  if (status == "open")
-    return void res
-      .status(404)
-      .setHeader("Content-Type", "text/plain")
-      .send("Session is already in use !")
-      .end();
+    return res.send("Invalid session ID !");
+
   res.setHeader("content-type", "image/png");
   res.send(await qrcode.toBuffer(QR_GENERATE));
 });
